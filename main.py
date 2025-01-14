35c23e728493aba634fce9399ad06b25b4f79e43
MvdZ-2489120 <m.h.vandezedde@students.uu.nl>
2025-01-14 12:36:20 +0100

    Version 2
    - Added InitializeCards() and InitializeTable() to make sure we can replay the game without restarting.
    
    -Made sure that the last card from the Cards list could be used as well, it used to give an error because you cant take an random.randint(0,0)
    
    -Fixed the Set_Check() and now prints the good sets with their corresponding letters on the screen and their property lists.
    
    -Added a function to find all the possible sets.
    
    -In Set_Try() made sure the function still works if the Cards List is empty
    
    -Added an input delay on the input for a to l
    
    -Changed the if that calls the Set_Try() function to only be called when Enter is pressed.
    
    -Added a feature that if Backspace is pressed the SET_SelectorList is emptied
    
    -Added a function that checks if there is a set and if not displays that on the screen and replaces 3 random cards with three new cards after a second.
    
    -Added a yellow glow over the selected cards.

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ screen = pygame.display.set_mode((950,550))
 pygame.display.set_caption('SET')
 clock=pygame.time.Clock()
 score=0
+GameOver=False
+inputdelay=0
 
 gameboard = pygame.Surface((950,550))
 gameboard.fill(pygame.Color(152, 155, 156))
@@ -21,7 +23,7 @@ Font=pygame.font.SysFont('Arial',16)
 #Shape, diamond=0, oval=1, squiggle=2
 #Color, green=0, purple=1, red=2
 
-#Creating the all the cards
+#Creating all the cards
 amount = {0:"1",1:"2",2:"3"}
 fill={0:"empty",1:"filled",2:"shaded"}
 shape={0:"diamond",1:"oval",2:"squiggle"}
@@ -29,31 +31,56 @@ color = {0:"green",1:"purple",2:"red"}
 
 #initializing all the cards
 Cards=[]
-for i in range(3):
-    for j in range (3):
-        for k in range(3):
-            for l in range(3):
-                Cards.append([i,j,k,l,"pygame"+"\\"+"kaarten"+"\\"+color[l]+shape[k]+fill[j]+amount[i]+".gif"])
+def InitializeTable():
+    for i in range(3):
+        for j in range (3):
+            for k in range(3):
+                for l in range(3):
+                    Cards.append([i,j,k,l,"pygame"+"\\"+"kaarten"+"\\"+color[l]+shape[k]+fill[j]+amount[i]+".gif"])
+InitializeTable()
 
 #getcard() retrieves a random card that hasnt been used yet
 def getcard():
-    randomnumber=random.randint(0,len(Cards)-1)
+    if len(Cards)>1:
+        randomnumber=random.randint(0,len(Cards)-1)
+    elif len(Cards)==1:
+        randomnumber=0
     temp=Cards[randomnumber]
     Cards.pop(randomnumber)
     return temp
 
 #initializing the table
 Table_Cards=[]
-for i in range(12):
-    Table_Cards.append(getcard())
+def InitializeTable():
+    for i in range(12):
+        Table_Cards.append(getcard())
+InitializeTable()
 
 def Set_Check(Kaart1,Kaart2,Kaart3): #function that checks for 3 given indices of cards in the table cards list if they are a set
-    for i in range(4): 
+    for i in range(4):
         #testing for each of the for properties if they are all the same or all different
     	#if that is not the case it returns False other and otherwise returns True
-        if not(Table_Cards[Kaart1][i]==Table_Cards[Kaart2][i]==Table_Cards[Kaart3][i] or Table_Cards[Kaart1][i]!=Table_Cards[Kaart2][i]!=Table_Cards[Kaart3][i]):
+        if Table_Cards[Kaart1][i]==Table_Cards[Kaart2][i] and Table_Cards[Kaart1][i]==Table_Cards[Kaart3][i] and Table_Cards[Kaart2][i]==Table_Cards[Kaart3][i]:
+            i=i
+        elif Table_Cards[Kaart1][i]!=Table_Cards[Kaart2][i] and Table_Cards[Kaart1][i]!=Table_Cards[Kaart3][i] and Table_Cards[Kaart2][i]!=Table_Cards[Kaart3][i]:
+            i=i
+        else:
             return False
+    print (chr(Kaart1+97) +" "+ str(Table_Cards[Kaart1]))
+    print (chr(Kaart2+97) +" "+  str(Table_Cards[Kaart2]))
+    print (chr(Kaart3+97) +" "+ str(Table_Cards[Kaart3]))
+    print(True)
     return True
+
+#Getting all the sets
+All_Sets=[]
+def Get_Sets():
+    for Kaart1 in range (10):
+        for Kaart2 in range(Kaart1+1,11):
+            for Kaart3 in range(Kaart2+1,12):
+                if Set_Check(Kaart1,Kaart2,Kaart3):
+                    All_Sets.append([Kaart1,Kaart2,Kaart3])
+Get_Sets()
 
 #List that has all the selected cards
 SET_SelectorList=[]
@@ -61,11 +88,19 @@ SET_SelectorList=[]
 #this functions handles the set selection of the player
 def Set_Try(s,a,b,c):
     if Set_Check(a,b,c):
-        Table_Cards[a]=getcard()
-        Table_Cards[b]=getcard()
-        Table_Cards[c]=getcard()
+        if len(Cards)>0:
+            Table_Cards[a]=getcard()
+            Table_Cards[b]=getcard()
+            Table_Cards[c]=getcard()
+        else:
+            Table_Cards.pop(a)
+            Table_Cards.pop(b)
+            Table_Cards.pop(c)
+        All_Sets.clear()
+        Get_Sets()
         return s+1
-    else:return s
+    else:
+        return s
 
 #keys dictionary which making checking all the inputs of the characters a to l way easier
 keysdictionary={0:pygame.K_a,1:pygame.K_b,2:pygame.K_c,3:pygame.K_d,4:pygame.K_e,5:pygame.K_f,6:pygame.K_g,7:pygame.K_h,8:pygame.K_i,9:pygame.K_j,10:pygame.K_k,11:pygame.K_l}     
@@ -81,15 +116,41 @@ while True:
     inputkeys=pygame.key.get_pressed()
 
     #checking if a to l are pressed
-    for i in range(12):
-        if inputkeys[keysdictionary[i]]:
-            if SET_SelectorList.count(i)<1:
-                SET_SelectorList.append(i)
+    if inputdelay==0:
+        if len(SET_SelectorList)<3:
+            for i in range(12):
+                if inputkeys[keysdictionary[i]]:
+                    if SET_SelectorList.count(i)<1:
+                        SET_SelectorList.append(i)
+                        inputdelay=10
+                        break
+    else:
+        inputdelay-=1
         
-    if len(SET_SelectorList)>=3:
-        score=Set_Try(score,SET_SelectorList[0],SET_SelectorList[1],SET_SelectorList[2])
+    if inputkeys[pygame.K_RETURN]: #when enter is pressed checking the set
+        if len(SET_SelectorList)==3: #when 3 cards are selected checking if they are a set.
+            score=Set_Try(score,SET_SelectorList[0],SET_SelectorList[1],SET_SelectorList[2])
+            SET_SelectorList=[]
+
+    if inputkeys[pygame.K_BACKSPACE]:
         SET_SelectorList=[]
 
+    NoSetsTextTime=0
+    if len(All_Sets)==0:
+        if len(Cards)>0:
+            if NoSetsTextTime>0:
+                NoSetsTextTime=60
+        else:
+            GameOver=True
+    
+    if NoSetsTextTime>0:
+        NoSets = Font.render("No Sets Available!",False,(0,0,0))
+        gameboard.blit(NoSets,(475-NoSets.get_width()//2,25-NoSets.get_height()//2))
+        NoSetsTextTime-=1
+        if NoSetsTextTime==0:    
+            Table_Cards[random.randint(0,3)]=getcard()
+            Table_Cards[random.randint(4,7)]=getcard()
+            Table_Cards[random.randint(8,11)]=getcard()
 
     #adding the cards to the screen
     for i in range(len(Table_Cards)):
@@ -98,12 +159,17 @@ while True:
         temporary_surface.blit(cardcharacter,(80,180))
         gameboard.blit(temporary_surface,(50+150*(i%6),50+250*(i//6)))
 
-
     #adding the gameboard to the screen
     screen.blit(gameboard,(0,0))
 
     scoretext=Font.render("score="+str(score),False,(0,0,0))
     screen.blit(scoretext,(20,20))
     
+    for i in SET_SelectorList:
+        temporary_surface = pygame.Surface((100,200))
+        temporary_surface.fill(pygame.Color(255,255,0))
+        temporary_surface.set_alpha(64)
+        screen.blit(temporary_surface,(50+150*(i%6),50+250*(i//6)))
+
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(240)
